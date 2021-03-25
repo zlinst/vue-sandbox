88554f9c07e2def2f998aed27d8e35acdd00c1bd
Lin <lin@caaus.me>
2021-03-25 22:18:01 +0800

    Add (missing) input component for boolean prop

---
+++ src/lib/inputs/InputBoolean.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="input-boolean">
+    <input id="toggle" type="checkbox" :checked="value" @change="update" />
+    <span class="text">{{ value ? 'Yes' : 'No' }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    update(e) {
+      this.$emit('input', e.target.checked)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input-boolean {
+  input {
+    transform: scale(1.5);
+    padding: 0.1em;
+  }
+
+  .text {
+    margin-left: 0.5em;
+  }
+}
+</style>
