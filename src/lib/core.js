455e267c7b5a5e1db6b2805c22a82ff7f9dd0b9e
Lin <lin@caaus.me>
2021-03-25 22:15:38 +0800

    WIP: Add/update various core & utility functions

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,14 +1,37 @@
-export const getInputComponent = (propType) => {
-  if (typeof propType === 'string') {
-    switch (propType.toLowerCase()) {
-      case 'string':
-        return () => import('./inputs/InputString.vue')
-    }
+/**
+ * Convert Vue prop type (e.g. String, Boolean etc) to lowercased string name for type checking.
+ */
+export const parsePropType = (propType) => {
+  if (typeof propType === 'function') {
+    return propType.name.toLowerCase()
+  } else if (typeof propType === 'string') {
+    return propType.toLocaleLowerCase()
+  } else {
+    return null
+  }
+}
+
+export const getPropInfo = (propType) => {
+  switch (parsePropType(propType)) {
+    case 'string':
+      return {
+        name: 'String',
+        component: () => import('./inputs/InputString.vue'),
+      }
+    case 'boolean':
+      return {
+        name: 'Boolean',
+        component: () => import('./inputs/InputBoolean.vue'),
+      }
   }
 
-  return () => import('./inputs/InputUnknown.vue')
+  return {
+    name: 'Unknown',
+    component: () => import('./inputs/InputUnknown.vue'),
+  }
 }
 
 export default {
-  getInputComponent,
+  parsePropType,
+  getPropInfo,
 }
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,7 @@
 export const has = (obj, key) => {
   return Object.prototype.hasOwnProperty.call(obj, key)
 }
+
+export const isArray = (obj) => {
+  return Array.isArray(obj)
+}
