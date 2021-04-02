34f96613164e79a8d8517eb53ed3c23cf38452b0
Lin <lin@caaus.me>
2021-04-02 20:55:02 +0800

    WIP: update ComponentProp

--- src/lib/ComponentProp.vue
+++ src/lib/ComponentProp.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="sandbox-prop">
     <div class="sandbox-prop__header">
-      <text-badge
-        class="sandbox-prop__name-badge sandbox-monofont"
-        size="sm"
-        v-text="name"
-      />
+      <text-badge class="sandbox-prop__name sandbox-monofont" v-text="name" />
       <div class="sandbox-prop__type-badges">
         <text-badge
           v-for="(typeInfo, index) in typeList"
@@ -16,10 +12,19 @@
             inactive: customInput,
           }"
           size="sm"
-          @click="() => inactive || switchPropType(index)"
+          @click="() => customInput || switchPropType(index)"
           v-text="typeInfo.name"
         />
       </div>
+    </div>
+    <div v-if="tagList.length" class="sandbox-prop__tags">
+      <text-badge
+        v-for="tag in tagList"
+        :key="tag.name"
+        class="sandbox-prop__tag sandbox-monofont"
+        :class="`sandbox-prop__tag__${tag.classMod}`"
+        v-text="tag.name"
+      />
     </div>
     <div class="sandbox-prop__input">
       <slot v-bind="{ valueProxy, attrs: $attrs }">
@@ -87,6 +92,13 @@ export default {
         this.$emit('input', value)
       },
     },
+    tagList() {
+      return [
+        ...(this.isModel ? [{ name: 'V-Model', classMod: 'vmodel' }] : []),
+        ...(this.required ? [{ name: 'Required', classMod: 'required' }] : []),
+        ...(this.unlisted ? [{ name: 'Unlisted', classMod: 'unlisted' }] : []),
+      ]
+    },
     typeList() {
       if (isArray(this.type)) {
         return this.type.map((t) => getPropInfo(t))
@@ -113,9 +125,10 @@ export default {
 .sandbox-prop__header {
   display: flex;
   align-items: center;
+  font-size: 0.85em;
 }
 
-.sandbox-prop__name-badge {
+.sandbox-prop__name {
   background-color: #264b6d;
 }
 
@@ -127,21 +140,42 @@ export default {
 
 .sandbox-prop__type-badge {
   margin: 0.15em;
+  background-color: #769cbf;
 
   &.highlighted {
-    background-color: lighten(#264b6d, 20%);
+    background-color: darken(#769cbf, 15%);
   }
   &:not(.inactive) {
     cursor: pointer;
   }
 }
 
-.sandbox-prop__input {
-  padding-top: 0.5em;
+.sandbox-prop__tags {
+  margin-top: 0.3em;
+  margin-bottom: 0.5em;
 }
 
-.sandbox-prop__input-unknown {
-  color: #999;
-  font-size: 0.8em;
+.sandbox-prop__tag {
+  font-size: 0.75em;
+  background-color: initial;
+  border: 1px solid;
+
+  &__vmodel {
+    color: #718bff;
+  }
+  &__required {
+    color: #ff6666;
+  }
+  &__unlisted {
+    color: #575757;
+  }
+
+  &:not(:first-child) {
+    margin-left: 0.5em;
+  }
+}
+
+.sandbox-prop__input {
+  margin-top: 0.5em;
 }
 </style>
