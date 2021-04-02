150400eb277887737d641994daabe8745c6b314f
Lin <lin@caaus.me>
2021-04-02 21:35:07 +0800

    Support string prop type

--- src/lib/shared.js
+++ src/lib/shared.js
@@ -12,6 +12,9 @@ export const isArray = (obj) => {
  * See: https://github.com/vuejs/vue/blob/fa1f81e91062e9de6161708209cd7354733aa354/src/core/util/props.js#L190
  */
 export const parsePropType = (propType) => {
+  // support custom type passed as string
+  if (typeof propType === 'string') return propType
+
   const match = propType && propType.toString().match(/^\s*function (\w+)/)
   return match ? match[1] : ''
 }
