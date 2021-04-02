ee25bc1a8ece69d79f8bf7143b7f74cef6d61811
Lin <lin@caaus.me>
2021-04-02 20:47:16 +0800

    Update parsePropType util function

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,30 +1,27 @@
 /**
  * Convert Vue prop type (e.g. String, Boolean etc) to string for type checking.
+ *
+ * See: https://github.com/vuejs/vue/blob/fa1f81e91062e9de6161708209cd7354733aa354/src/core/util/props.js#L190
  */
 export const parsePropType = (propType) => {
-  if (typeof propType === 'function') {
-    return propType.name
-  } else if (typeof propType === 'string') {
-    return propType
-  } else {
-    return null
-  }
+  const match = propType && propType.toString().match(/^\s*function (\w+)/)
+  return match ? match[1] : ''
 }
 
 export const getPropInfo = (propType) => {
   const propTypeName = parsePropType(propType)
-  switch (propTypeName?.toLowerCase()) {
-    case 'string':
+  switch (propTypeName) {
+    case 'String':
       return {
         name: 'String',
         component: () => import('./inputs/InputString.vue'),
       }
-    case 'boolean':
+    case 'Boolean':
       return {
         name: 'Boolean',
         component: () => import('./inputs/InputBoolean.vue'),
       }
-    case 'number':
+    case 'Number':
       return {
         name: 'Number',
         component: () => import('./inputs/InputNumber.vue'),
