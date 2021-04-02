51eb8a76acc0719af12d11f4e5c4ca50f10ff85a
Lin <lin@caaus.me>
2021-04-02 20:49:10 +0800

    Simplify TextBadge component

--- src/lib/misc/TextBadge.vue
+++ src/lib/misc/TextBadge.vue
@@ -1,9 +1,5 @@
 <template>
-  <span
-    class="sandbox-badge"
-    :class="dynamicClass"
-    @click="(e) => $emit('click', e)"
-  >
+  <span class="sandbox-badge" @click="(e) => $emit('click', e)">
     <slot>{{ text }}</slot>
   </span>
 </template>
@@ -15,36 +11,18 @@ export default {
       type: String,
       default: undefined,
     },
-    size: {
-      type: String,
-      default: 'md',
-    },
-  },
-  computed: {
-    dynamicClass() {
-      return 'sandbox-badge__size__' + this.size
-    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .sandbox-badge {
+  font-size: inherit;
   padding: 0.15em 0.35em;
   font-size: inherit;
   border-radius: 0.3em;
 
   color: white;
   background-color: #555;
-}
-
-.sandbox-badge__size__sm {
-  font-size: 0.8em;
-}
-.sandbox-badge__size__md {
-  font-size: inherit;
-}
-.sandbox-badge__size__lg {
-  font-size: 1.2em;
 }
 </style>
