0b93a5f4ef83d8799b55ea8bd0e739eede4b76e8
Lin <lin@caaus.me>
2021-04-03 17:10:45 +0800

    Update configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  ignorePatterns: ["dist"],
 }
