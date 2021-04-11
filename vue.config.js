d67ae35fd552af77ec5c54b62fc983b456c24015
Lin <lin@caaus.me>
2021-04-11 09:30:45 +0800

    Update webpack alias to make dev easier

--- vue.config.js
+++ vue.config.js
@@ -9,10 +9,11 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@lib': path.resolve(
-          __dirname,
-          isDev ? 'lib' : 'dist/vue2-sandbox.umd.js'
-        ),
+        // make @lib alised to self when testing in production mode, so import will be
+        // the umd build specified in package.main
+        '@lib': path.resolve(__dirname, isDev ? 'lib' : ''),
+        // we use require.context to load library components for testing, and the UMD build only
+        // expose necessary componnets for the runtime, so we need @lib-src for such purpose
         '@lib-src': path.resolve(__dirname, 'lib'),
       },
     },
