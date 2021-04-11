f90629652e76b8c19b24136d5915e846b430b531
Lin <lin@caaus.me>
2021-04-11 15:30:12 +0800

    Add new script to build demo site

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
+  margin: auto;
+  max-width: 1200px;
 }
 
 .vue-sandbox-monofont {
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 
-const isDev = process.env.NODE_ENV === 'development'
-
 module.exports = {
   css: {
     extract: false,
@@ -11,7 +9,10 @@ module.exports = {
       alias: {
         // make @lib alised to self when testing in production mode, so import will be
         // the umd build specified in package.main
-        '@lib': path.resolve(__dirname, isDev ? 'lib' : ''),
+        '@lib': path.resolve(
+          __dirname,
+          process.env.VUE_SANDBOX_ENV === 'production' ? '' : 'lib'
+        ),
         // we use require.context to load library components for testing, and the UMD build only
         // expose necessary componnets for the runtime, so we need @lib-src for such purpose
         '@lib-src': path.resolve(__dirname, 'lib'),
